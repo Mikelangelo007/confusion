917e0833847cdb05c9d4e4947740577a323541d8
Elchin <appwebgame@mail.ru>
2021-09-26 20:38:47 +0300

    finish

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+     $(document).ready(function(){
+         $('#mycarousel').carousel({interval:2000});
+         $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+     });
+     
+  
+//      $(document).ready(function(){
+//          $('#mycarousel').carousel({interval:2000});
+//          $('#carousel-pause').click(function(){ */}
+//              $('#mycarousel').carousel('pause');
+//          });
+//          $('#carousel-play').click(function(){
+//              $('#mycarousel').carousel('cycle');
+//          });
+//      });
+//      $(document).ready(function(){
+//          $('[data-toggle="tooltip"').tooltip();
+//      })
+     $("#knopka").click(function(){
+        $('#theReserveForm').modal('toggle')
+
+     })
+     $("#loginKnopkasi").click(function(){
+        $('#loginModal').modal('toggle')
+     })
